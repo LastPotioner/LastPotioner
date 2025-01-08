5e927e28a6573c335a7d1ba8ccd5e48425a4d6d1
StarDust504 <86709115+StarDust504@users.noreply.github.com>
2025-01-08 23:20:35 +1000

    switched to engine version 5.5

--- Source/LastPotionerEditor.Target.cs
+++ Source/LastPotionerEditor.Target.cs
@@ -2,14 +2,20 @@
 
 using UnrealBuildTool;
 using System.Collections.Generic;
+//using EpicGames.Horde.Users;
+//using Microsoft.CodeAnalysis.Diagnostics;
 
 public class LastPotionerEditorTarget : TargetRules
 {
 	public LastPotionerEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+        DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+        CppStandard = CppStandardVersion.Cpp20;
 
-		ExtraModuleNames.AddRange( new string[] { "LastPotioner" } );
+
+
+        ExtraModuleNames.AddRange( new string[] { "LastPotioner" } );
 	}
 }
